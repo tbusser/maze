80078f677ecf046696ec6e87e956034cbf5d774c
Thijs Busser <tbusser@gmail.com>
2017-11-21 22:51:15 +0100

    Add comments to utility methods

--- src/scripts/utilities.js
+++ src/scripts/utilities.js
@@ -16,27 +16,40 @@ function getRandomInt(min, max) {
 }
 
 /**
+ * Checks if the provided value is undefined or null.
  *
+ * @param {any} value The value to check.
  *
- * @param {any} value
- * @returns
+ * @returns {Boolean} Returns true when the value is undefined or null;
+ *          otherwise the result is false.
  */
 function isNil(value) {
 	return (value == null);
 }
 
 /**
- *
+ * Checks if the provided value is undefined or null. When this isn't the case
+ * the method continues to check if the value has a length or size property and
+ * if this property is 0.
  *
  * @param {any} value
- * @returns
+ *
+ * @returns {Boolean} When the value is nullish or has a length or size property
+ *          whose value is 0, the result is true; otherwise the result is false.
  */
 function isNilOrEmpty(value) {
 	if (isNil(value)) {
 		return true;
 	}
 
-	if (value.hasOwnProperty('length') && value.length === 0) {
+	// Use "prop in value" instead of "value.hasOwnProperty(prop)", it is not
+	// important if the property comes from the prototype or not. In case of
+	// Map, "<Map>.hasOwnProperty(size)" fails but "size in <Map>" works.
+	if ('length' in value && value.length === 0) {
+		return true;
+	}
+
+	if ('size' in value && value.size === 0) {
 		return true;
 	}
 
